1cd3e3c359a4f42114f9b459a43694fe8241e2e2
hemanthnakka <89064707+hemanthnakka@users.noreply.github.com>
2021-08-23 10:57:43 +0200

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,6 +1,8 @@
 class Simple{
 public static void main(String args[]) throws InterruptedException{
 Thread.sleep(10000);
-System.out.println("Hello java updates");
+System.out.println("Hello java updated 2nd time");
+  system.out.println("Hello Java added to branch1");
+  System.out.println("Hello Java added to branch1 to check sleep");
 }
 }
